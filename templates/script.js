c88dab17dbde7240480714c48f388f633e91827c
alnj <kokone@takat.su>
2021-03-09 11:18:45 +0100

    fix indentation

--- templates/script.js
+++ templates/script.js
@@ -3,23 +3,23 @@ let maxComments = 20
 const convertSeconds = (x) => new Date(x * 1000).toISOString().substr(11, 8)
 const makeComment = (json, container) => {
   const profile =  `https://twitcasting.tv/${json.id}`
-	const commentHTML = `
-		<div class="comment">
-			<div class="avatar">
-				<a class="profileLink" href="${profile}">
-					<img class="avatar-img" src="${json.avatar}" alt="${json.id}">
-				</a>
-			</div>
-			<div class="message">
-				<div class="author">
-					<a class="author-link" href="${profile}">${json.nick}</a>
-					<span class="timestamp">${convertSeconds(json.timestamp)}</span>
-				</div>
-				<div class="text">${json.text}</div>
-			</div>
-		</div>
-	`
-	container.insertAdjacentHTML('beforeend', commentHTML)
+  const commentHTML = `
+    <div class="comment">
+      <div class="avatar">
+        <a class="profileLink" href="${profile}">
+          <img class="avatar-img" src="${json.avatar}" alt="${json.id}">
+        </a>
+      </div>
+      <div class="message">
+        <div class="author">
+          <a class="author-link" href="${profile}">${json.nick}</a>
+          <span class="timestamp">${convertSeconds(json.timestamp)}</span>
+        </div>
+        <div class="text">${json.text}</div>
+      </div>
+    </div>
+  `
+  container.insertAdjacentHTML('beforeend', commentHTML)
 }
 
 window.addEventListener('load', () => {
@@ -30,7 +30,6 @@ window.addEventListener('load', () => {
   vid.addEventListener('timeupdate', () => {
     let time = vid.currentTime
     if (Math.abs(oldTime - time) < 1) return
-    // for (const el of visible) el.remove()
     container.innerHTML = ''
     const shown = comments.filter(x => x.timestamp <= time).slice(0, maxComments)
     shown.forEach(x => makeComment(x, container))
