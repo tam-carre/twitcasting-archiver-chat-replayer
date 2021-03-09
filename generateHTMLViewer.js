d05e65b531e00397545de48174b7a585278e5c62
alnj <kokone@takat.su>
2021-03-09 18:14:27 +0100

    fix accidental paste

--- generateHTMLViewer.js
+++ generateHTMLViewer.js
@@ -13,7 +13,7 @@ const chatFile = path.join(dir, `${name}.chat.jsonl`)
 const metadataFile = path.join(dir, `${name}.info.json`)
 const outputName = `${name}.html`
 const outputFile = path.join(dir, outputName)
-let g:html_indent_style1 = "inc"
+
 // Script
 console.log(`Writing HTML viewer to ${outputFile}.`)
 if (fs.existsSync(outputFile)) fs.unlinkSync(outputFile)
@@ -92,7 +92,7 @@ readStream.on('data', (line) => {
 // Append footer
 readStream.on('end', () => {
 	const script = fs.readFileSync('templates/script.js')
-	const pageFooter = html`
+	const pageFooter = `
           ]
 					${script}
         </script>
